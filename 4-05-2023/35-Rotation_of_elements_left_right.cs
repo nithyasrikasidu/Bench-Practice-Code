cdf4a418ff2014eb43895b557fc26c51f3bac9fc
Nithya <115216293+nithyasrikasidu@users.noreply.github.com>
2023-05-11 10:07:43 +0530

    Update 35-Rotation_of_elements_left_right.cs

--- 4-05-2023/35-Rotation_of_elements_left_right.cs
+++ 4-05-2023/35-Rotation_of_elements_left_right.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Programs
 {
     class _35_Rotation_of_elements_left_right
-    {
+    { 
         public static void Main()
         {
             Console.WriteLine("Enter the length of an array");
@@ -17,8 +17,29 @@ namespace Programs
             for(int i=0;i<arr.Length;i++)
             {
                 arr[i] = Convert.ToInt32(Console.ReadLine());
-            }    
-            
+            }
+            _35_Rotation_of_elements_left_right array = new _35_Rotation_of_elements_left_right();
+            array.LeftRotation(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+
+        }
+
+
+
+        private void LeftRotation(int[] arr)
+        {
+            int x = arr[0];
+            for (int i = 0; i < (arr.Length - 1); i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            arr[(arr.Length - 1)] = x;
         }
     }
+    
 }
